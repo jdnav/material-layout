fcef78eb1fa6a8ae46f9e4c342c34d7e6c9e112d
jnavarrosierra <jnavarrosierra@deloitte.es>
2020-03-13 22:47:58 +0100

    routes mock-up ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // path: 'route-one',
+    // component: /route-oneComponent, // Parent component must be set if "outlet" property appears!
+    // loadChildren: './/route-one//route-one.module#/route-oneModule',
+    // canActivate: [AuthGuard],
+    // data: { shouldReuse: false }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
