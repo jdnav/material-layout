63000d71520fab0422126f516fbd0c1866ef8d90
jnavarrosierra <jnavarrosierra@deloitte.es>
2020-05-09 20:12:31 +0200

    Routes empty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [
-  {
-    // path: 'route-one',
-    // component: /route-oneComponent, // Parent component must be set if "outlet" property appears!
-    // loadChildren: './/route-one//route-one.module#/route-oneModule',
-    // canActivate: [AuthGuard],
-    // data: { shouldReuse: false }
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
